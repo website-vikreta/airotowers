f164a198df251270c84c0cb857336f1af419bb9b
sbgprojects <shubham.gujarathi@decartem.com>
2021-02-25 11:10:19 +0530

    A

--- js/main.js
+++ js/main.js
@@ -45,13 +45,13 @@ $(document).ready(function () {
 
 function sendmail() {
     // e.preventDefault();
+    $("#submit-btn").attr("disabled", true);
 
     var name = $("#contact-name").val();
     var email = $("#contact-email").val();
     var number = $("#contact-number").val();
     var message = $("#contact-message").val();
     var body = "<b>Name</b> : " + name + "<br> <b>Email</b> : " + email + "<br> <b>Contact</b>: " + number + "<br> <b>Message</b>: " + message;
-
 
 
     Email.send({
@@ -66,6 +66,7 @@ function sendmail() {
         message => {
             if (message == "OK") {
                 alert("We got your message. We will reach out to you very soon.");
+                $("#submit-btn").attr("disabled", false);
                 $("#contact-form").trigger("reset");
             } else {
                 alert(message);
